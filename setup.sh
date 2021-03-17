6371ffb40c42b8b8edb6a8f85d839c5f87ed6749
kirshiyin89 <70095657+kirshiyin89@users.noreply.github.com>
2021-03-17 22:25:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "
-[server]\n
-headless = true\n
-enableCORS=false\n
-port = $PORT\n
-\n
+echo "\
+[general]\n\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
